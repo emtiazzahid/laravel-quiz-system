b5ee8d2ec194f38484e49d2e14689b10f76e03bb
Emtiaz Zahid <emtiazzahid@gmail.com>
2021-09-12 09:47:57 +0600

    mcq index api

--- app/Http/Controllers/MCQController.php
+++ app/Http/Controllers/MCQController.php
@@ -11,7 +11,11 @@ class MCQController extends BaseController
 {
     private $mcq;
 
-    public function __construct(MCQInterface $mcq){
+    /**
+     * @param MCQInterface $mcq
+     */
+    public function __construct(MCQInterface $mcq)
+    {
         $this->mcq = $mcq;
     }
 
@@ -26,6 +30,10 @@ class MCQController extends BaseController
         );
     }
 
+    /**
+     * @param MCQRequest $request
+     * @return \Illuminate\Http\JsonResponse
+     */
     public function store(MCQRequest $request)
     {
         $data = $this->mcq->create($request->all());
@@ -33,21 +41,38 @@ class MCQController extends BaseController
         return $this->sendResponse($data, 'Data Insert Successfully');
     }
 
+    /**
+     * @param $id
+     * @return mixed
+     */
     public function show($id)
     {
         return $this->mcq->getById($id);
     }
 
+    /**
+     * @param $id
+     * @return mixed
+     */
     public function edit($id)
     {
         return $this->mcq->getById($id);
     }
 
+    /**
+     * @param $id
+     * @param MCQRequest $request
+     * @return mixed
+     */
     public function update($id, MCQRequest $request)
     {
         return $this->mcq->update($id, $request->all());
     }
 
+    /**
+     * @param $id
+     * @return mixed
+     */
     public function destroy($id)
     {
         return $this->mcq->delete($id);
--- app/Http/Resources/MCQResource.php
+++ app/Http/Resources/MCQResource.php
@@ -14,17 +14,12 @@ class MCQResource extends JsonResource
      */
     public function toArray($request)
     {
+        $option = 'option_' . $this->correct_answer_no;
         return [
             'id' => $this->id,
-            'name' => $this->question,
-            'option_1' => $this->option_1,
-            'option_2' => $this->option_2,
-            'option_3' => $this->option_3,
-            'option_4' => $this->option_4,
-            'option_5' => $this->option_5,
-            'correct_answer_no' => $this->correct_answer_no,
-            'created_at' => $this->created_at,
-            'updated_at' => $this->updated_at,
+            'question' => $this->question,
+            'correct_answer' => $this->$option,
+            'created_at' => $this->created_at
         ];
     }
 }
